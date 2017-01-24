bf7a1127721cb9eeb672ecd58d761303b11e8988
Faheel Ahmad <faheel@live.in>
2017-01-24 23:51:35 +0530

    Add SortingUtils.h

---
+++ Sorting/SortingUtils.h
@@ -0,0 +1,70 @@
+/*
+    Various utility functions (for input, output etc.)
+    used in programs that implement sorting algorithms
+*/
+
+#ifndef IOSTREAM_H
+#include <iostream>
+#endif
+
+#ifndef VECTOR_H
+#include <vector>
+#endif
+
+using namespace std;
+
+// Input utils:
+
+void getInputSize(size_t& size) {
+    cout << "Enter the input size : ";
+    cin >> size;
+}
+
+void getInputValues(vector<int>& values, const size_t& size) {
+    cout << "Enter " << size << " integers :\n";
+    for (int& val: values)
+        cin >> val;
+    cin.ignore();
+}
+
+void getOrder(int& order, string& orderText ) {
+    cout << "In which order should the values be sorted?\n";
+    cout << "[A]scending / [d]escending : ";
+    getline(cin, orderText);
+
+    if (orderText[0] == 'd' or orderText[0] == 'D') {
+        order = -1;
+        orderText = "descending";
+    }
+    else {    // ascending order by default
+        order = 1;
+        orderText = "ascending";
+    }
+}
+
+void getWhetherToShowState(bool& toShowState) {
+    string answer;
+    cout << "Would you like to view the state of the values after each iteration?\n";
+    cout << "[y]es / [N]o : ";
+    getline(cin, answer);
+
+    toShowState = false;        // by default, don't show state
+    if (answer[0] == 'y' or answer[0] == 'Y')    // unless user asks for it
+        toShowState = true;
+}
+
+// Output utils:
+
+void displayState(const vector<int>& values) {
+    for (const int& val: values)
+        cout << val << ' ';
+    cout << '\n';
+}
+
+// Other utils:
+
+void swap(int& a, int& b) {
+    int temp = a;
+    a = b;
+    b = temp;
+}
