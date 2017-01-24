7b30a830e57132c35c6a869dfd64b99de2286886
Faheel Ahmad <faheel@live.in>
2017-01-24 22:31:13 +0530

    Format indentation

--- Sorting/QuickSort.cpp
+++ Sorting/QuickSort.cpp
@@ -1,6 +1,6 @@
 /*
-	Quicksort:
-	An efficient, comparison-based, in-place, divide and conquer sorting algorithm
+    Quicksort:
+    An efficient, comparison-based, in-place, divide and conquer sorting algorithm
 */
 
 #include <algorithm>    // for swap()
@@ -12,27 +12,27 @@
 using namespace std;
 
 void displayState(const vector<int>& valuesVect) {
-	for (const int& value: valuesVect)
-		cout << value << ' ';
-	cout << '\n';
+    for (const int& value: valuesVect)
+        cout << value << ' ';
+    cout << '\n';
 }
 
 size_t partition(vector<int>& values, const size_t start, const size_t end, const int order) {
     // choose a random index between start & end
     size_t randomIndex = start + (rand() % (end - start + 1));
 
-	// swap the value there with the first value in given range
+    // swap the value there with the first value in given range
     swap(values[randomIndex], values[start]);
 
-	// make that value the pivot element
+    // make that value the pivot element
     int pivot = values[start];
 
     size_t i = start + 1;
     for (size_t j = start + 1; j <= end; j++)  {
-	    /*
-			place elements which are less than the pivot on one side,
-			and those which are greater on the other
-		*/
+        /*
+            place elements which are less than the pivot on one side,
+            and those which are greater on the other
+        */
         if (order * values[j] < order * pivot) {
             swap(values[i], values[j]);
             i++;
@@ -46,59 +46,59 @@ size_t partition(vector<int>& values, const size_t start, const size_t end, cons
 }
 
 void quickSort(vector<int>& values, const int start, const int end, const int order, const bool askedToViewState) {
-	if (start < end) {
-		size_t pivotIndex = partition(values, start, end, order);
+    if (start < end) {
+        size_t pivotIndex = partition(values, start, end, order);
 
-		// sort values to the left of pivot
-		quickSort(values, start, pivotIndex-1, order, askedToViewState);
+        // sort values to the left of pivot
+        quickSort(values, start, pivotIndex-1, order, askedToViewState);
 
-		// sort values to the right of pivot
-		quickSort(values, pivotIndex+1, end, order, askedToViewState);
+        // sort values to the right of pivot
+        quickSort(values, pivotIndex+1, end, order, askedToViewState);
 
-		if (askedToViewState)
-			displayState(values);
-	}
+        if (askedToViewState)
+            displayState(values);
+    }
 }
 
 int main() {
-	int size;
-	cout << "Enter the input size : ";
-	cin >> size;
+    int size;
+    cout << "Enter the input size : ";
+    cin >> size;
 
-	vector<int> inputVect(size);
-	cout << "Enter " << size << " integers :\n";
-	for (int& val: inputVect)
-		cin >> val;
-	cin.ignore();
+    vector<int> inputVect(size);
+    cout << "Enter " << size << " integers :\n";
+    for (int& val: inputVect)
+        cin >> val;
+    cin.ignore();
 
-	string orderInput;
-	cout << "In which order should the values be sorted?\n[A]scending / [d]escending : ";
-	getline(cin, orderInput);
-	int order;
-	if (orderInput[0] == 'd' or orderInput[0] == 'D') {
-		order = -1;
-		orderInput = "descending";
-	}
-	else {	// ascending order by default
-		order = 1;
-		orderInput = "ascending";
-	}
+    string orderInput;
+    cout << "In which order should the values be sorted?\n[A]scending / [d]escending : ";
+    getline(cin, orderInput);
+    int order;
+    if (orderInput[0] == 'd' or orderInput[0] == 'D') {
+        order = -1;
+        orderInput = "descending";
+    }
+    else {    // ascending order by default
+        order = 1;
+        orderInput = "ascending";
+    }
 
-	string answer;
-	cout << "Would you like to view the state of the values after each iteration?\n[y]es / [N]o : ";
-	getline(cin, answer);
-	bool askedToViewState = false;		// by default, don't show state
-	if (answer[0] == 'y' or answer[0] == 'Y')	// unless user asks for it
-		askedToViewState = true;
+    string answer;
+    cout << "Would you like to view the state of the values after each iteration?\n[y]es / [N]o : ";
+    getline(cin, answer);
+    bool askedToViewState = false;        // by default, don't show state
+    if (answer[0] == 'y' or answer[0] == 'Y')    // unless user asks for it
+        askedToViewState = true;
 
-	cout << '\n';
+    cout << '\n';
     srand(time(0));     // seed PRNG
-	quickSort(inputVect, 0, size-1, order, askedToViewState);
+    quickSort(inputVect, 0, size-1, order, askedToViewState);
 
-	cout << "\nThe values in " << orderInput << " order are :\n";
-	for (const int& val: inputVect)
-		cout << val << " ";
-	cout << "\n";
+    cout << "\nThe values in " << orderInput << " order are :\n";
+    for (const int& val: inputVect)
+        cout << val << " ";
+    cout << "\n";
 
-	return 0;
+    return 0;
 }
