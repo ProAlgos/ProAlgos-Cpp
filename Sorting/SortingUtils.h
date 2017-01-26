8a713b7ddf5665362dbf68895a836a0dcd52372c
mangu93 <adrianmarinportillo@gmail.com>
2017-01-26 23:57:08 +0100

    Suggestions used to fix the issue

--- Sorting/SortingUtils.h
+++ Sorting/SortingUtils.h
@@ -12,7 +12,7 @@
 #include <cstdlib>
 
 #endif
-#define EXIT_CODE_ERROR -1
+#define EXIT_CODE_NOT_SORTED 2
 using namespace std;
 
 // Input utils:
@@ -20,12 +20,12 @@ using namespace std;
 void getInputSize(size_t& size) {
     cout << "Enter the input size : ";
     cin >> size;
-    if(size <0) {
+    if((int) size <0) {
         cout << "Invalid input size! Try again.\n";
         getInputSize(size);
     }else if(size == 0) {
         cout << "There is nothing to sort here\n";
-        exit(EXIT_CODE_ERROR );
+        exit(EXIT_CODE_NOT_SORTED );
     }
 }
 
