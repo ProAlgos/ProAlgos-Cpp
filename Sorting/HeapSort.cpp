c9ecf1ee7a73cca168fc83321a3d70b85b4350e3
Satch97 <satchin.campbell@gmail.com>
2017-02-09 19:52:58 -0500

    Added Descending Functionality

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -6,45 +6,47 @@
 using namespace std;
 
 
-void maxheapify(vector<int> &heap,int parent,int size){
+void heapify(vector<int> &heap,int parent,int size, int order){
   int child = (parent*2)+1, temp;
   while(child<size){//travels down
-    if(heap[child]<heap[child+1]&&(child+1<size)){
-      child++;//child refers to the larger of the two children
+      displayState(heap);
+    if((order*heap[child])<(order*heap[child+1])&&(child+1<size)){
+      child++;//child refers to the larger/smaller of the two children
     }
-    if(heap[parent]<heap[child]){
-      temp = heap[parent];
-      heap[parent] = heap[child];
-      heap[child] = temp;
+    else{}
+    if((order*heap[parent])<(order*heap[child])){
+      swap(heap[parent],heap[child]);
     }
     parent = child;
-    child= (parent*2)+1;
+    child=(parent*2)+1;
   }
 }
-void heapsort(vector<int> &heap,int size){
+void heapsort(vector<int> &heap,int size, int order){
   int temp,n = size;
   while(n>0){
     temp = heap[n-1];
     heap[n-1] = heap[0];
     heap[0]=temp;
     n-=1;
-    maxheapify(heap,0,n);
+    heapify(heap,0,n, order);
   }
 }
-void make_heap(vector<int> &heap, int size){
+void make_heap(vector<int> &heap, int size, int order){
     for(int x = (size/2)-1; x>=0;x--){ //Searches and reheaps all non-leaf node
-    maxheapify(heap, x, size);
+    heapify(heap, x, size, order);
   }
 }
-
 int main(){
     size_t size;
     getInputSize(size);
     vector<int> values(size);
     getInputValues(values, size);
-    //get order here when main code complete
-    make_heap(values, size);
-    heapsort(values, size);
+    int order;
+    string orderText;
+    getOrder(order, orderText);
+    make_heap(values, size, order);
+    heapsort(values, size, order);
+    cout << "The result in " + orderText + " order is as follows : " << endl;
     displayState(values);
     return 0;
 }
