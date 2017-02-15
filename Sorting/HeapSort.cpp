880332dc8289876d7c69eda1a3c11924b5339c77
Satch97 <satchin.campbell@gmail.com>
2017-02-15 13:01:50 -0500

    Added sortingutils.h functions

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -6,7 +6,7 @@
 using namespace std;
 
 
-void heapify(vector<int> &heap,int parent,int size, int order){
+void heapify(vector<int> &heap,int parent,int size, int order, bool showState){
   int child = (parent*2)+1, temp;
   while(child<size){//travels down
       displayState(heap);
@@ -19,21 +19,21 @@ void heapify(vector<int> &heap,int parent,int size, int order){
     }
     parent = child;
     child=(parent*2)+1;
+    displayState(heap);
   }
 }
-void heapsort(vector<int> &heap,int size, int order){
+void heapsort(vector<int> &heap,int size, int order,bool showState){
   int temp,n = size;
   while(n>0){
-    temp = heap[n-1];
-    heap[n-1] = heap[0];
-    heap[0]=temp;
+    swap(heap[0], heap[n-1]);
     n-=1;
-    heapify(heap,0,n, order);
+    heapify(heap,0,n, order, showState);
   }
 }
-void make_heap(vector<int> &heap, int size, int order){
+void make_heap(vector<int> &heap, int size, int order, bool showState){
     for(int x = (size/2)-1; x>=0;x--){ //Searches and reheaps all non-leaf node
-    heapify(heap, x, size, order);
+    heapify(heap, x, size, order, showState);
+    if(showState) displayState(heap);
   }
 }
 int main(){
@@ -44,8 +44,15 @@ int main(){
     int order;
     string orderText;
     getOrder(order, orderText);
-    make_heap(values, size, order);
-    heapsort(values, size, order);
+    bool showState;
+    getWhetherToShowState(showState);
+    if(showState) cout << endl << "Showing stages of making heap" << endl;
+    make_heap(values, size, order, showState);
+    if (showState){
+        cout << "Initial Heap Has been Made" << endl << endl;
+        cout <<"Now Showing State During Heapify Operation" << endl;
+    }
+    heapsort(values, size, order, showState);
     cout << "The result in " + orderText + " order is as follows : " << endl;
     displayState(values);
     return 0;
