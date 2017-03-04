5db9d55e306ebc091ece1868fd914e37b15b07a3
Satch97 <satchin.campbell@gmail.com>
2017-03-04 13:19:47 -0500

    Adjusted spacing

--- Sorting/HeapSort.cpp
+++ Sorting/HeapSort.cpp
@@ -1,6 +1,6 @@
 /*
     Heap Sort :
-    Uses an array-implemented heap to sort list of numbers.
+    Uses an array-implemented heap to sort a list of numbers.
 
     Time complexity:
     O(N * log(N)), where n is the number of elements to be sorted.
@@ -24,18 +24,18 @@ void heapify(vector<int> &heap, int parent, const int size, const int order, con
        if ((order * heap[child]) < (order * heap[child+1]) and (child+1 < size)){
            child++;//child refers to the larger/smaller of the two children
         }
-        if ((order * heap[parent])<(order * heap[child])){
+        if ((order * heap[parent]) < (order * heap[child])){
             swap(heap[parent],heap[child]);//if the parent is smaller(order==ascending) or larger(order==descending), then swap
         }
     parent = child;
-    child = (parent*2)+1;
+    child = (parent*2) + 1;
     if (showState) displayState(heap);
     }
 }
 //removes largest/smallest node, replaces it and reheapifies with new heap.
 void heapsort(vector<int> &heap, const int size, const int order, const bool showState){
     int temp, last = size;
-    while (last>0){
+    while (last > 0){
         swap(heap[0], heap[last-1]);
         last-=1;
         heapify(heap,0,last, order, showState);
@@ -43,7 +43,7 @@ void heapsort(vector<int> &heap, const int size, const int order, const bool sho
 }
 //Makes ordered heap by "heapify"ing from highest indexed non-leaf node(curr_node) to the top node, index[0]
 void make_heap(vector<int> &heap, const int size, const int order, const bool showState){
-    for (int curr_node = (size/2)-1; curr_node>=0;curr_node--){
+    for (int curr_node = (size/2) - 1; curr_node >= 0;curr_node--){
         heapify(heap, curr_node, size, order, showState);
         if (showState) displayState(heap);
     }
