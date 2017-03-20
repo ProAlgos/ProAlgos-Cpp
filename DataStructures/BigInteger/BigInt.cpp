d408eb1bbd109a513a57118e150dfb7f5226a3ff
SeanHouser <seanmhouser@gmail.com>
2017-03-19 19:23:22 -0500

    Progress
    Patrially tested existing implementation with some corrections
    - Utility functions (with correction for detecting signed strings as
    valid)
    - Constructors
    - IO operations
    - Assignment operations

--- DataStructures/BigInteger/BigInt.cpp
+++ DataStructures/BigInteger/BigInt.cpp
@@ -11,10 +11,20 @@ void notify_invalid_input(const std::string& input);
 int _tmain(int argc, _TCHAR* argv[])
 {
 
-	BigInt bI;
 	std::string s = "10";
+	BigInt large_int(s);
+	BigInt large_int2(large_int);
+	BigInt int_test(1000000);
+	BigInt str_test = "-666";
+	BigInt int_test2 = -777;
+	
+	
+	BigInt cin_test;
+	std::cout << "Please enter number for BigInt cin_test: ";
+	std::cin >> cin_test;
+
 	if (is_valid_number(s)) {
-		std::cout << "Your number is: " << s << '\n';
+		std::cout << "Your number is: " << cin_test << '\n';
 	}
 	else
 		notify_invalid_input(s);
@@ -26,11 +36,11 @@ int _tmain(int argc, _TCHAR* argv[])
     -----------------
 */
 
-//in: passed string. out: 
 bool is_valid_number(const std::string& num) {
     for (char digit : num)
         if (digit < '0' || digit > '9')
-            return false;
+			if (digit != '+' && digit != '-')
+				return false;
 
     return true;
 }
@@ -49,16 +59,13 @@ BigInt::BigInt() {
     sign = '+';
 }
 
-
-/*
-
 BigInt::BigInt(const BigInt& num) {
     value = num.value;
     sign = num.sign;
 }
 
 BigInt::BigInt(const std::string num) {
-    if (num[0] == '+' or num[0] == '-') {     // check for sign
+    if (num[0] == '+' || num[0] == '-') {     // check for sign
         std::string magnitude = num.substr(1);
         if (is_valid_number(magnitude)) {
             value = magnitude;
@@ -81,6 +88,7 @@ BigInt::BigInt(const std::string num) {
     }
 }
 
+
 BigInt::BigInt(const long long num) {
     value = std::to_string(num);
     if (num < 0) {
@@ -92,9 +100,31 @@ BigInt::BigInt(const long long num) {
 }
 
 
+/*
+	I/O stream operators
+	--------------------
+*/
 
-//    Assignment operators
+std::istream& operator>>(std::istream& in, BigInt& num) {
+	std::string input;
+	in >> input;
+	num = BigInt(input);  // remove sign from value and set sign, if exists
+
+	return in;
+}
+
+std::ostream& operator<<(std::ostream& out, const BigInt& num) {
+    if (num.sign == '-')
+        out << num.sign;
+    out << num.value;
+
+    return out;
+}
+
+/*
+    Assignment operators
     --------------------
+*/
 
 
 void BigInt::operator=(const BigInt& num) {
@@ -114,7 +144,7 @@ void BigInt::operator=(const std::string num) {
     sign = temp.sign;
 }
 
-
+/*
 
 //    Arithmetic operators
     --------------------
@@ -254,27 +284,7 @@ bool BigInt::operator!=(const long long num) {
     return true;
 }
 
-
-
-//    I/O stream operators
-    --------------------
-
-
-std::istream& operator>>(std::istream& in, BigInt& num) {
-    std::string input;
-    in >> input;
-    num = BigInt(input);  // remove sign from value and set sign, if exists
-
-    return in;
-}
-
-std::ostream& operator<<(std::ostream& out, const BigInt& num) {
-    if (num.sign == '-')
-        out << num.sign;
-    out << num.value;
-
-    return out;
-}
+*/
 
 #ifdef DEBUG
 int main() {
@@ -285,6 +295,6 @@ int main() {
     if (-num == 51311531)
         std::cout << "Big integer : " << num << "\n";
 }
+
 #endif
 
-*/
