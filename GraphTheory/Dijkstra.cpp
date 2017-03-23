f0fdd2fec7393e112cf086ca148fb023323627b9
Rasul Kerimov <yoatom96@hotmail.com>
2017-03-23 05:50:15 -0700

    Update Dijkstra.cpp

--- GraphTheory/Dijkstra.cpp
+++ GraphTheory/Dijkstra.cpp
@@ -1,17 +1,22 @@
+/*
+	Dijkstra Algorithm
+	Finding Shortest Paths from Given Vertex to all Other Vertices.
+	Time Complexity: 
+	O(M log N), M is the number of edges and N is the number of vertices in a graph
+	Space Complexity: O(M), M is the number of edges in a graph.
+*/
 #include <iostream>
 #include <vector>
 #include <queue>
 
-#define INF 2147483647
+const int INF = INT_MAX;
 
 using namespace std;
 
+typedef vector<vector< pair<int, int>>> Graph; 
 
-vector<vector< pair<int, int>>> g; // global graph
-int vertices, edges;
-
-vector<int> dijkstra(const int &start) {
-	vector<int> distances(g.size(), INF);
+vector<int> dijkstra(const int &start, const Graph& graph, const int& vertices, const int& edges) {
+	vector<int> distances(graph.size(), INF);
 	distances[start] = 0;
 	priority_queue<pair<int,int>> queue; // we hold vertices in queue
 	queue.push({0, start});
@@ -22,11 +27,14 @@ vector<int> dijkstra(const int &start) {
 		queue.pop();
 
 		if(dist > distances[value]) continue;
-		for(int i = 0; i < g[value].size(); ++i) {
-			int to = g[value][i].first, dist_temp = g[value][i].second;
+		for(int i = 0; i < graph[value].size(); ++i) {
+			int to = graph[value][i].first, dist_temp = graph[value][i].second;
+			
 			if(distances[value] + dist_temp < distances[to]) {
+				
 				distances[to] = distances[value] + dist_temp;
 				queue.push({-distances[to], to});
+			
 			}
 		}
 	}
@@ -35,21 +43,24 @@ vector<int> dijkstra(const int &start) {
 
 int main() {
 	ios_base::sync_with_stdio(0);
+	int vertices, edges;
 	cout << "Enter the number of vertices and edges : " << endl;
 	cin >> vertices >> edges;
-
-	g.resize(vertices + 1);
+	
+	Graph graph;
+	graph.resize(vertices + 1);
 
 	cout << "Now enter your edges in format: \"u v w\". Where u and w are vertices and w is the distance between them." << endl;
 	for(int i = 0; i < edges; ++i) {
 		int temp_start, temp_end, temp_distance;
 		cin >> temp_start >> temp_end >> temp_distance;
-		g[temp_start].push_back({temp_end, temp_distance});
+		graph[temp_start].push_back({temp_end, temp_distance});
 	}
 
-	vector<int> d = dijkstra(1);
+	vector<int> min_distances = dijkstra(1, graph, vertices, edges);
 	//cout << "Distances from the first vertix to the rest : " << endl;
-    for(int i = 2; i <= vertices; ++i)
-        cout << "Distance from 1 to " << i << " is equal to : " << d[i] << endl;
-
+    	for(int i = 2; i <= vertices; ++i)
+        	cout << "Distance from 1 to " << i << " is equal to : " << min_distances[i] << endl;
+	
+	return 0;
 }
