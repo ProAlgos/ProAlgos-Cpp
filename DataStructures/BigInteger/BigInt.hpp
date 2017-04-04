1ec9ca268e032043a59196157f9b621aa63de72a
SeanHouser <seanmhouser@gmail.com>
2017-04-04 14:32:21 -0500

    Revert "Revert "Multiplication operations on signed BigInts complete with testing""
    This reverts commit 38c239a807d600c6f64b1358a7411b3447e5a21b.

--- DataStructures/BigInteger/BigInt.hpp
+++ DataStructures/BigInteger/BigInt.hpp
@@ -24,8 +24,8 @@ public:
     BigInt operator+(long long);
     void operator-(const BigInt&);  // TODO
     void operator-(long long);      // TODO
-    void operator*(const BigInt&);  // TODO
-    void operator*(long long);      // TODO
+    BigInt operator*(const BigInt&); 
+    BigInt operator*(long long);      
     void operator/(const BigInt&);  // TODO
     void operator/(long long);      // TODO
     void operator%(const BigInt&);  // TODO
@@ -36,8 +36,8 @@ public:
     BigInt operator+=(long long);
     void operator-=(const BigInt&); // TODO
     void operator-=(long long);     // TODO
-    void operator*=(const BigInt&); // TODO
-    void operator*=(long long);     // TODO
+    BigInt operator*=(const BigInt&); 
+    BigInt operator*=(long long);     
     void operator/=(const BigInt&); // TODO
     void operator/=(long long);     // TODO
     void operator%=(const BigInt&); // TODO
