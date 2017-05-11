1c8e88008ad344b3f09227dbcb4e83e4ec2a9ad9
Davide Spataro <davide90.spataro@gmail.com>
2017-05-11 12:33:00 +0200

    Add Recursion folder and TowerOfHanoi algo

--- Recursion/TowerHanoi.cpp
+++ Recursion/TowerHanoi.cpp
@@ -1,9 +1,32 @@
+/*
+@Author Davide Spataro www.davidespataro.it
+
+Tower Of Hanoi
+-------------------
+Solves the tower of hanoi problem ( https://en.wikipedia.org/wiki/Tower_of_Hanoi )
+using a recursive approach.
+
+
+Time complexity
+---------------
+O(2^n),where n is the number of disks
+
+Space complexity
+----------------
+O(n), where n is the number of disks 
+(not considering the stack space for the required by recursion). 
+*/
+
+
+
 #include <iostream>
 #include<array>
 #include<vector>
 #include <stack>
 #include <unistd.h>
 using namespace std;
+
+
 
 int ndisks;
 int sleeptime = 9000;
@@ -22,7 +45,7 @@ void drawHanoi(auto& piles) {
   for (int  i = ndisks ; i >= 0; --i) {
     for (int j = 0; j < 3; j++) {
       printf(" | ");
-      if (piles[j].size() > i) {
+      if (piles[j].size() > (unsigned)i) {
         int n = piles[j][i] + 1;
         printNtimes('=', n);
         printNtimes(' ', sep[j] - n + 1);
@@ -40,7 +63,7 @@ void drawHanoi(auto& piles) {
 
 
 /**
-  Function that solves the towe of hanoi problem
+  Function that solves the tower of hanoi problem
 */
 void solveHanoi(int start, int end, int from, int to, auto& piles) {
   //which pile should we use as support pile?
@@ -66,7 +89,6 @@ void solveHanoi(int start, int end, int from, int to, auto& piles) {
 
 
 int main() {
-
 
   int np;
   cout << "How many disks?...\n";
