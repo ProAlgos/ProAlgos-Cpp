1e24f5bd04a920590747113db535de4aa4766e4f
Davide Spataro <davide90.spataro@gmail.com>
2017-05-11 12:22:21 +0200

    Add Recursion folder and TowerOfHanoi algo

---
+++ Recursion/TowerHanoi.cpp
@@ -0,0 +1,88 @@
+#include <iostream>
+#include<array>
+#include<vector>
+#include <stack>
+#include <unistd.h>
+using namespace std;
+
+int ndisks;
+int sleeptime = 9000;
+int nmoves = 0;
+
+//prints the current status of the piles
+void drawHanoi(auto& piles) {
+
+  auto printNtimes = [](char c, int n) {
+    for (int i = 0 ; i < n; i++)
+      printf("%c", c);
+  };
+
+  std::array<int,3> sep= {ndisks, ndisks, ndisks};
+
+  for (int  i = ndisks ; i >= 0; --i) {
+    for (int j = 0; j < 3; j++) {
+      printf(" | ");
+      if (piles[j].size() > i) {
+        int n = piles[j][i] + 1;
+        printNtimes('=', n);
+        printNtimes(' ', sep[j] - n + 1);
+      } else
+        printNtimes(' ', sep[j] + 1);
+
+      printf(" | ");
+    }
+    printf("\n");
+  }
+  printf("\n\n");
+
+  usleep(sleeptime);
+}
+
+
+/**
+  Function that solves the towe of hanoi problem
+*/
+void solveHanoi(int start, int end, int from, int to, auto& piles) {
+  //which pile should we use as support pile?
+  int support = (from + to) % 4;
+  support = support == 0 ? 2 : support;
+
+  if (end - start == 0 ) {
+
+    piles[to - 1].push_back(piles[from - 1].back());
+    piles[from - 1].pop_back();
+    drawHanoi(piles);
+    nmoves++;
+  }
+
+  else {
+    solveHanoi(start, end - 1, from, support, piles);
+    solveHanoi(end, end, from, to, piles);
+    solveHanoi(start, end - 1, support, to, piles);
+  }
+
+}
+
+
+
+int main() {
+
+
+  int np;
+  cout << "How many disks?...\n";
+  cin >> np;
+  np--;
+
+  vector<vector<int>> piles(3, vector<int>());
+  ndisks = np;
+
+  //initialize all disks on pile number one
+  for (int i = np; i >= 0; i--)
+    piles[0].push_back(i);
+
+  solveHanoi(0, np, 1, 3, piles);
+
+  printf("solved using %d moves\n", nmoves);
+
+  return 0;
+}
