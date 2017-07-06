124cb595b8ac7d1ed851b23abbc5b2883b43ee9c
Ritwik Sharma <ritwik.1204@gmail.com>
2017-07-06 21:45:48 +0530

    reduced characters per line

--- Sorting/BucketSort.cpp
+++ Sorting/BucketSort.cpp
@@ -1,7 +1,9 @@
 /*
     Bucket sort
     ----------
-    Bucket sort, or bin sort, is a sorting algorithm that works by distributing the elements of an array into a number of buckets. 
+    Bucket sort, or bin sort, 
+    is a sorting algorithm that works by 
+    distributing the elements of an array into a number of buckets. 
     Each bucket is then sorted individually, 
     either using a different sorting algorithm, 
     or by recursively applying the bucket sorting algorithm.
