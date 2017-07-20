c4255a0ba0222936d4e3baaaf42c087cf2f7910d
Ahmad Hussain <ahmad.hussain.307@gmail.com>
2017-07-20 15:11:04 +0200

    Updated FastPower.test.cpp

--- Test/NumberTheory/FastPower.test.cpp
+++ Test/NumberTheory/FastPower.test.cpp
@@ -7,17 +7,17 @@
 
 TEST_CASE("Base cases", "[fast_power]") {
     //REQUIRE(fast_power(0,0) == undefined);
-    REQUIRE(fast_power(0,1,ULLONG_MAX) == 0);
-    REQUIRE(fast_power(1,0,ULLONG_MAX) == 1);
-    REQUIRE(fast_power(1,1,ULLONG_MAX) == 1);
+    REQUIRE(fast_power(0,1) == 0);
+    REQUIRE(fast_power(1,0) == 1);
+    REQUIRE(fast_power(1,1) == 1);
 }
 
 TEST_CASE("Normal cases", "[fast_power]") {
-    REQUIRE(fast_power(2,2,ULLONG_MAX) == 4);
-    REQUIRE(fast_power(2,4,ULLONG_MAX) == 16);
-    REQUIRE(fast_power(3,4,ULLONG_MAX) == 81);
-    REQUIRE(fast_power(7,9,ULLONG_MAX) == 40353607);
-    REQUIRE(fast_power(15,10,ULLONG_MAX) == 576650390625);
+    REQUIRE(fast_power(2,2) == 4);
+    REQUIRE(fast_power(2,4) == 16);
+    REQUIRE(fast_power(3,4) == 81);
+    REQUIRE(fast_power(7,9) == 40353607);
+    REQUIRE(fast_power(15,10) == 576650390625);
 }
 
 TEST_CASE("Overflow cases", "[fast_power]") {
