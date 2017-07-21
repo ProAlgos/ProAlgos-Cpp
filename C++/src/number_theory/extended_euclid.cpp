7f2ed260cc8feb57cde29617856a56c5adf5d8e3
Faheel Ahmad <faheel@live.in>
2017-07-21 05:39:21 +0530

    Refactor Extended Euclidean algorithm

--- C++/src/number_theory/extended_euclid.cpp
+++ C++/src/number_theory/extended_euclid.cpp
@@ -1,36 +1,29 @@
 /*
-    Extended Euclidean Algorithm
+    Extended Euclidean algorithm
     ----------------------------
+    Given two non-negative numbers A and B as inputs, find M[0], M[1] such that:
+    gcd(A, B) = M[0]*B + M[1]*B
 
-    Given two non-negative numbers a & b as inputs, find M[0], M[1] such that
-
-    gcd(a, b) = M[0]*a + M[1]*b
-
-    This algorithm has a variety of uses - in particular, it can be used to
-    quickly compute multiplicative inverses in finite fields, as is often
-    needed for cryptographic algorithms
-
-    Time Complexity
+    Time complexity
     ---------------
     O(log(K)), where K = min(A, B) is the smaller of the input integers A and B
 
-    Space Complexity
+    Space complexity
     ----------------
-    O(1)   
+    O(1).
 */
 
-#include<iostream>
-#include<array>
+#include <iostream>
+#include <array>
 
 using namespace std;
 
 /*
-
     extended_euclidean
-    --------------
+    ------------------
     Uses the recurrence relation,
         GCD(a, b) = GCD(b, a%b),
-    to find coefficients M[0] and M[1] such that 
+    to find coefficients M[0] and M[1] such that
         GCD(a, b) = M[0]*a + M[1]*b
 
     Return value
@@ -47,23 +40,20 @@ using namespace std;
 
 */
 
-array<int,2> extended_euclidean(int a, int b)
-{
+array<int, 2> extended_euclidean(int a, int b) {
     //loop invariant: gcd(a,b) is the same at the end of each iteration
     //a = aM[0]*(original value of a) + aM[1]*(original value of b)
     //the above statement holds for b and bM as well
-
-    array<int,2> aM = {1,0};
-    array<int,2> bM = {0,1};
-    while(b!=0)
-    {
-        aM[0] -= (a/b)*bM[0];
-        aM[1] -= (a/b)*bM[1];
+    array<int, 2> aM = {{1, 0}};
+    array<int, 2> bM = {{0, 1}};
+    while (b != 0) {
+        aM[0] -= (a/b) * bM[0];
+        aM[1] -= (a/b) * bM[1];
 
         aM.swap(bM);
 
         int newA = b;
-        b = a%b;
+        b = a % b;
         a = newA;
 
     }
@@ -76,7 +66,6 @@ array<int,2> extended_euclidean(int a, int b)
     ----------------
     Gets a positive integer as input from the user for a variable, giving it a
     name as specified.
-
 */
 
 void get_positive_int(int& var, const string& var_name) {
@@ -89,25 +78,15 @@ void get_positive_int(int& var, const string& var_name) {
     } while (var < 0);
 }
 
-/*
-    main
-    ----
-*/
-
-#ifndef TEST
-int main()
-{
-
-    int a;
-    int b;
+int main() {
+    int a, b;
     get_positive_int(a, "a");
     get_positive_int(b, "b");
 
-    array<int,2> M = extended_euclidean(a, b);
+    array<int, 2> M = extended_euclidean(a, b);
     int gcd = M[0] * a + M[1] * b;
     cout << "gcd(" << a << "," << b << ") = " << M[0] << "*" << a << " + " << M[1] << "*" << b;
     cout << " = " << gcd <<"\n";
 
     return 0;
 }
-#endif
