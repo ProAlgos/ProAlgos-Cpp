7bce61b39a6c07a605a4de5ef05f0bb7c3757288
Ryan Young <young2r2@mail.uc.edu>
2017-09-08 10:22:07 -0400

    Create dynamic_array.test.cpp

---
+++ C++/test/data_structures/dynamic_array.test.cpp
@@ -0,0 +1,15 @@
+#define DYNAMIC_ARRAY
+#define CATCH_CONFIG_MAIN
+
+#include "third_party/catch.hpp"
+#include "source/path_to/source_file.cpp"
+
+// Add your test cases here:
+#ifndef DYNAMIC_ARRAY
+
+
+#endif
+
+
+// don't write anything after this line!
+#undef NAME_OF_FILE_TEST
