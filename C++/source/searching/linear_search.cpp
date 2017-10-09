4aecba1bbeee5999380c25f7669fb51dba96fb73
Syed Faraaz Ahmad <faraaz98@live.com>
2017-10-09 22:41:33 +0530

    add tests

--- C++/source/searching/linear_search.cpp
+++ C++/source/searching/linear_search.cpp
@@ -18,10 +18,11 @@
 
 using namespace std;
 
-size_t linear_search(const int value, const vector<int> &vect) {
+template <typename T>
+size_t linear_search(const T value, const vector<T> &vect) {
 
     // Search all the elements in the array
-    for(unsigned i = 0; i < vect.size(); i++) {
+    for(size_t i = 0; i < vect.size(); i++) {
         // If the element at index i matches the target value
         // return the position i
         if(vect[i] == value) {
@@ -29,8 +30,8 @@ size_t linear_search(const int value, const vector<int> &vect) {
         }
     }
 
-    // If no match is found, return -1
-    return -1;
+    // If no match is found, return 0
+    return 0;
 }
 
 #ifndef LINEAR_SEARCH_TEST
@@ -48,10 +49,10 @@ int main() {
     for(int& val: inputVect)
         cin >> val;
 
-    int index = linear_search(value, inputVect);
+    int position = linear_search(value, inputVect);
     cout << "\n";
-    if(index != -1)
-        cout << "Found " << value << " at position " << index << "\n";
+    if(position != 0)
+        cout << "Found " << value << " at position " << position << "\n";
     else
         cout << value << " is not present among the input values\n";
 
