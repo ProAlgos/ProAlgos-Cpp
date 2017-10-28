f6b32fe7a0af1dd781566c2d2d3660d14e075df0
Faheel Ahmad <faheel@live.in>
2017-10-28 14:08:32 +0530

    Refactor linear search

--- C++/source/searching/linear_search.cpp
+++ C++/source/searching/linear_search.cpp
@@ -1,16 +1,16 @@
 /*
-    Linear Search
+    Linear search
     -------------
-    A searching algorithm that finds the position of a target value
-    within an array (sorted or otherwise).
+    A searching algorithm that finds the position of a target value within an
+    array (sorted or otherwise).
 
     Time complexity
-    --------------
-    O(N), where N is the number of elements in the array
+    ---------------
+    O(N), where N is the number of elements in the array.
 
-    Space Complexity
-    --------------
-    O(1)
+    Space complexity
+    ----------------
+    O(1).
 */
 
 #include <iostream>
@@ -19,43 +19,36 @@
 using namespace std;
 
 template <typename T>
-size_t linear_search(const T value, const vector<T> &vect) {
+int linear_search(const T element, const vector<T>& values) {
+    for (size_t i = 0; i < values.size(); i++)
+        if (values[i] == element)   // it's a match!
+            return i;       // return the index at which it was found
 
-    // Search all the elements in the array
-    for(size_t i = 0; i < vect.size(); i++) {
-        // If the element at index i matches the target value
-        // return the position i
-        if(vect[i] == value) {
-            return i + 1;
-        }
-    }
-
-    // If no match is found, return 0
-    return 0;
+    return -1;  // when no match is found
 }
 
 #ifndef LINEAR_SEARCH_TEST
 int main() {
-    int value;
-    cout << "Enter the value to search for: ";
-    cin >> value;
+    int element;
+    cout << "Enter the element to search for : ";
+    cin >> element;
 
-    int size;
-    cout << "Enter the input size: ";
+    size_t size;
+    cout << "Enter the input size : ";
     cin >> size;
 
-    vector<int> inputVect(size);
-    cout << "Enter " << size << " integers: ";
-    for(int& val: inputVect)
-        cin >> val;
+    vector<int> input_values(size);
+    cout << "Enter " << size << " integers:\n";
+    for (int& value: input_values)
+        cin >> value;
 
-    int position = linear_search(value, inputVect);
+    int index = linear_search(element, input_values);
     cout << "\n";
-    if(position != 0)
-        cout << "Found " << value << " at position " << position << "\n";
+    if (index != -1)
+        cout << "Found " << element << " at index " << index << ".\n";
     else
-        cout << value << " is not present among the input values\n";
+        cout << "Did not find " << element << " among the input values.\n";
 
     return 0;
 }
-#endif
+#endif  // LINEAR_SEARCH_TEST
