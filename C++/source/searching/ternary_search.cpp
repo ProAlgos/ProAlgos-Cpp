6188a8c635be18a513e053f056b4e45e93d8b671
Faheel Ahmad <faheel@live.in>
2017-12-05 03:46:11 +0530

    Refactor ternary search

--- C++/source/searching/ternary_search.cpp
+++ C++/source/searching/ternary_search.cpp
@@ -1,7 +1,8 @@
 /*
-    Ternary Search
+    Ternary search
     --------------
-    A searching algorithm that finds the position of a maximum(minimum) value within an unimodal array (in 0-indexation).
+    A searching algorithm that finds the position of a maximum(minimum) value
+    within an unimodal array.
 
     Time complexity
     ---------------
@@ -17,60 +18,87 @@
 
 using namespace std;
 
-int ternarySearch(const vector<int>& Vect, const bool pattern) {
+enum Pattern {
+    ASCEND_THEN_DESCEND,
+    DESCEND_THEN_ASCEND
+};
+
 /*
-    The function needs to be given an array and a bool 'pattern', where:
-    pattern = false, if values in array are ascending, then descending, while their indices are ascending. If pattern equals "false", the function finds the position of a maximum value.
-    pattern = true, if values in array are descending, then ascending, while their indices are ascending. If pattern equals "true", the function finds the position of a minimum value.
+    ternary_search
+    -------------
+    If the values first ascend and then descend, this function finds the position
+    of the maximum value. Otherwise, if they first descend and then ascend, it
+    finds the position of the minimum value.
 */
-    int left = 0;
-    int right = (int)Vect.size() - 1;
-    //left and right are the edges of the interval of search.
+template <typename T>
+size_t ternary_search(const vector<T>& values, const Pattern& pattern) {
+    // left and right are the edges of the interval of search
+    size_t left = 0;
+    size_t right = values.size() - 1;
+
     bool changed = true;
-    while (right - left > 1 && changed) { //if the interval is not shrinking, its size already equals O(1).
+    size_t mid1, mid2;
+    while (right - left > 1 and changed) {  // if the interval is not shrinking,
+                                            // its size already equals O(1)
         changed = false;
-        int mid1 = left + (right - left) / 3;
-        int mid2 = right - (right - left) / 3;
-        if ((pattern && Vect[mid1] < Vect[mid2]) || (!pattern && Vect[mid1] > Vect[mid2])) {
+        mid1 = left + (right - left) / 3;
+        mid2 = right - (right - left) / 3;
+        if ((pattern and values[mid1] < values[mid2])
+            or (!pattern and values[mid1] > values[mid2])) {
             changed |= (right != mid2);
             right = mid2;
-        } else {
+        }
+        else {
             changed |= (left != mid1);
             left = mid1;
         }
     }
-    int min_value = Vect[left];
-    int min_index = left;
-    int max_value = Vect[left];
-    int max_index = left;
-    for (int index = left + 1; index <= right; index++) {
-        if (min_value > Vect[index]) {
-            min_value = Vect[index];
+
+    T min_value = values[left];
+    T max_value = values[left];
+    size_t min_index = left;
+    size_t max_index = left;
+    for (size_t index = left + 1; index <= right; index++) {
+        if (min_value > values[index]) {
+            min_value = values[index];
             min_index = index;
         }
-        if (max_value < Vect[index]) {
-            max_value = Vect[index];
+        if (max_value < values[index]) {
+            max_value = values[index];
             max_index = index;
         }
     }
-    if (pattern) return min_index; else return max_index;
+
+    return pattern == ASCEND_THEN_DESCEND ? max_index : min_index;
 }
 
 #ifndef TERNARY_SEARCH_TEST
-int main()
-{
-    int sz;
-    cout << "Enter the input size:\n";
-    cin >> sz;
-    vector <int> inputVect(sz); //supposely unimodal array
-    cout << "Enter array values:\n";
-    for (int& val : inputVect)
-        cin >> val;
-    bool pattern;
-    cout << "Enter 0 if the values in input are ascending then descending, 1 in other case: \n";
-    cin >> pattern;
-    int index = ternarySearch(inputVect, pattern);
-    if (pattern) cout << "Minimum value has position " << index << "\n"; else cout << "Maximum value has position " << index << "\n";
+int main() {
+    int size;
+    cout << "Enter the input size : ";
+    cin >> size;
+
+    vector <int> input_values(size);    //supposedly unimodal array
+    cout << "Enter " << size << " values:\n";
+    for (int& value: input_values)
+        cin >> value;
+
+    string answer;
+    cout << "\nDo the values first ascend then descend?"
+         << "If otherwise, enter \"no\".\n"
+         << "[Y]es / [n]o : ";
+    getline(cin, answer);
+
+    Pattern pattern = ASCEND_THEN_DESCEND;  // answer is "yes" by default
+    if (answer[0] == 'n' or answer[0] == 'N')   // user answers "no"
+        pattern = Pattern::DESCEND_THEN_ASCEND;
+
+    int index = ternary_search(input_values, pattern);
+    if (pattern == ASCEND_THEN_DESCEND)
+        cout << "Maximum value has position " << index << "\n";
+    else
+        cout << "Minimum value has position " << index << "\n";
+
     return 0;
 }
 #endif
