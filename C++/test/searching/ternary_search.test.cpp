70d057461155dfd85949012a4a245e1c31aa4e74
Faheel Ahmad <faheel@live.in>
2017-12-05 03:46:24 +0530

    Refactor ternary search test

--- C++/test/searching/ternary_search.test.cpp
+++ C++/test/searching/ternary_search.test.cpp
@@ -1,20 +1,26 @@
 #define TERNARY_SEARCH_TEST
 #define CATCH_CONFIG_MAIN
 
-#include "third_party/catch.hpp"
-#include "source/searching/ternary_search.cpp"
 #include <vector>
 #include <algorithm>
 
+#include "source/searching/ternary_search.cpp"
+#include "third_party/catch.hpp"
+
+/*
+    TODO: refactor
+*/
+
 unsigned int cur = 0;
 unsigned int a = 13674324, b = 1233;
+
 unsigned int nextRand24() {
     cur = cur * a + b;
     return cur >> 8;
 }
 
-vector <int> nextRandUnimodal(int sz, bool pattern) {
-    vector <int> res;
+vector<int> nextRandUnimodal(int size, const bool pattern) {
+    vector<int> res;
     res.clear();
     int prev = nextRand24() % 1000;
     if (!pattern) prev *= -1;
@@ -22,11 +28,11 @@ vector <int> nextRandUnimodal(int sz, bool pattern) {
     int md;
     bool was = false;
     if (!pattern) md = 1; else md = -1;
-    for (int i = 0; i < sz; i++) {
+    for (int i = 0; i < size; i++) {
         int del = (nextRand24() % 15) * md;
         prev += del;
         res.push_back(prev);
-        if (i > sz / 2 + (int)nextRand24() % 15 && !was) {
+        if (i > size / 2 + (int)nextRand24() % 15 && !was) {
             md *= -1;
             was = true;
         }
@@ -34,42 +40,50 @@ vector <int> nextRandUnimodal(int sz, bool pattern) {
     return res;
 }
 
-
-int solve(vector <int> &Vect, bool pattern) {
-    int val, ans;
+size_t get_expected_index(const vector<int>& values, const bool pattern) {
+    int val, expected_index;
     if (!pattern) {
         val = -1e9;
-        for (int i = 0; i < (int)Vect.size(); i++) {
-            if (val < Vect[i]){
-                val = Vect[i];
-                ans = i;
+        for (size_t i = 0; i < values.size(); i++) {
+            if (val < values[i]){
+                val = values[i];
+                expected_index = i;
             }
         }
     } else {
         val = 1e9;
-        for (int i = 0; i < (int)Vect.size(); i++) {
-            if (val > Vect[i]){
-                val = Vect[i];
-                ans = i;
+        for (size_t i = 0; i < values.size(); i++) {
+            if (val > values[i]){
+                val = values[i];
+                expected_index = i;
             }
         }
     }
-    return ans;
+    return expected_index;
 }
 
-TEST_CASE("Base cases", "[ternary search]") {
-    REQUIRE(ternarySearch(vector <int> ({-5}), false) == 0);
-    REQUIRE(ternarySearch(vector <int> ({5}), true) == 0);
-    REQUIRE(ternarySearch(vector <int> ({4, 5}), false) == 1);
-    REQUIRE(ternarySearch(vector <int> ({4, 5}), true) == 0);
+TEST_CASE("Base cases", "[searching][ternary_search]") {
+    REQUIRE(ternary_search(vector<int>({5}),  ASCEND_THEN_DESCEND) == 0);
+    REQUIRE(ternary_search(vector<int>({5}),  DESCEND_THEN_ASCEND) == 0);
+    REQUIRE(ternary_search(vector<int>({-5}), ASCEND_THEN_DESCEND) == 0);
+    REQUIRE(ternary_search(vector<int>({-5}), DESCEND_THEN_ASCEND) == 0);
+
+    REQUIRE(ternary_search(vector<int>({4, 5}), ASCEND_THEN_DESCEND) == 1);
+    REQUIRE(ternary_search(vector<int>({4, 5}), DESCEND_THEN_ASCEND) == 0);
 }
 
-TEST_CASE("Integer cases", "[ternary search]") {
-    for (int i = 0; i < 10; i++) {
-        bool pattern = bool(nextRand24() % 2);
-        vector <int> a = nextRandUnimodal(nextRand24() % 1000, pattern);
-        int answer = solve(a, pattern);
-        REQUIRE(ternarySearch(a, pattern) == answer);
+TEST_CASE("Normal cases", "[searching][ternary_search]") {
+    vector<int> values;
+    Pattern pattern;
+    size_t expected_index;
+    for (int test = 0; test < 20; test++) {
+        if (bool(nextRand24() % 2))
+            pattern = DESCEND_THEN_ASCEND;
+        else
+            pattern = ASCEND_THEN_DESCEND;
+        values = nextRandUnimodal(nextRand24() % 1000, pattern);
+        expected_index = get_expected_index(values, pattern);
+        REQUIRE(ternary_search(values, pattern) == expected_index);
     }
 }
 
