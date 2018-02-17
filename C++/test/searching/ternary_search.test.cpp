3ef0e0103219bf5a0cdb6dfc2928b2cd60894a1b
Faheel Ahmad <faheel@live.in>
2018-02-17 13:04:12 +0530

    Use namespace std in tests

--- C++/test/searching/ternary_search.test.cpp
+++ C++/test/searching/ternary_search.test.cpp
@@ -1,8 +1,7 @@
-#include <vector>
 #include <algorithm>
 
-#include "algorithms/searching/ternary_search.hpp"
 #include "third_party/catch.hpp"
+#include "algorithms/searching/ternary_search.hpp"
 
 using namespace std;
 
