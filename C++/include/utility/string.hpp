a90a625d6d8fa55ec555203acc3aefb61ff3809e
Utkarsh Chauhan <chauhanutkarsh@icloud.com>
2018-03-06 15:54:06 +0530

    now str is not modified at every iteration (added a new variable prev_pos)

--- C++/include/utility/string.hpp
+++ C++/include/utility/string.hpp
@@ -1,15 +1,15 @@
 /*
 	Split string Function
 	----------------------
-	Breaks a given sentence into individual parts (namely words) defined
+	Breaks a given string into individual parts (namely words) defined
 	by a character (named delimiter) and returns all the splitted words
 	as a vector of string.
-	The function takes two arguments as input a string sentence and a
+	The function takes two arguments as input a string str and a
 	character delimiter (Eg: ' ', '-', 'c', '!').
 
 	Time complexity
 	---------------
-	O(n), where n is the length of the string.
+	O(n^2), where n is the length of the string.
 
 	Space complexity
 	----------------
@@ -20,28 +20,17 @@
 #include <string>
 #include <vector>
 
-std::vector<std::string> split_string(std::string sentence, char delimiter)
+std::vector<std::string> split_string( const std::string& str, char delimiter)
 {
    std::vector<std::string> words;      //vector to  store the splitted words.
-   int pos;     //stores the position of the delimiter.
+   size_t pos, prev_pos = -1;     //stores the position of the delimiter.
 
     do
     {
-      pos = sentence.find(delimiter);
-      words.push_back(sentence.substr(0,pos));
-      sentence = sentence.substr(pos+1);
+      pos = str.find(delimiter, prev_pos + 1);
+      words.push_back(str.substr(prev_pos + 1, pos - prev_pos - 1));
+      prev_pos = pos;
     }while(pos != std::string::npos);
 
     return words;       //returns all the splited parts.
-}
-
-
-int main(){
-	std::string test = "I knew you were trouble";
-	std::vector<std::string> res = split_string(test, ' ');
-
-	for(int i = 0; i < res.size(); i++){
-		std::cout<<res[i]<<std::endl;
-	}
-	return 0;
 }
