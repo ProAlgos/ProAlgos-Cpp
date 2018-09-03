d2650c03b6eb710fae7198b3a3480bf538893de9
Stefan Ivanovski <ivanovskis@rocketmail.com>
2018-09-03 22:36:35 +0200

    Reorganised bubble sort functionality into header file

---
+++ C++/include/sorting/bubble_sort.h
@@ -0,0 +1,9 @@
+#ifndef BUBBLE_SORT_H
+#define BUBBLE_SORT_H
+
+#include <vector>
+
+class BubbleSort() {
+public:
+	void bubble_sort(std::vector<int>& values, const int order = 1, const bool to_show_state = false);
+};
