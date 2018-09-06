e043ddfa72109d8a505f871cf70292e4476214b5
shikherjaiswal <2015csb1116@iitrpr.ac.in>
2018-09-07 02:57:39 +0530

    added the requested changes and included more test cases

--- C++/include/algorithm/dynamic_programming/longest_increasing_subsequence.hpp
+++ C++/include/algorithm/dynamic_programming/longest_increasing_subsequence.hpp
@@ -1,44 +1,61 @@
-
 /*
-  Longest Increasing Subsequence
-  ------------------------------
-  Given an unsorted array of integers, return the longest increasing subsequence length
-  Time complexity
-  --------------
-  O(N^2), where N is size of array
-  Space complexity
-  ---------------
-  O(N), where N is size of array
-  Author
-  ------
-  Shikhar Jaiswal(@shikherjaiswal)
+      Longest Increasing Subsequence
+      ------------------------------
+      Given an unsorted array of integers, return the longest increasing subsequence length
+
+      Time complexity
+      --------------
+      O(N^2), where N is size of array
+
+      Space complexity
+      ---------------
+      O(N), where N is size of array
+
+      Author
+      ------
+      Shikhar Jaiswal(@shikherjaiswal)
 */
+
 #ifndef longest_increasing_subsequence_hpp
 #define longest_increasing_subsequence_hpp
+
 #include <vector>
-int lis( std :: vector< int > arr, int n ) {
-    int *lis, i, j, max = 0;
-    lis = (int*) malloc ( sizeof( int ) * n );
-    /* Initialize LIS values for all indexes */
-    for (i = 0; i < n; i++ ) {
+
+/*
+    ==========================================================================
+    Public interface
+    ==========================================================================
+*/
+
+
+/*
+    lis
+    ----------------
+    Returns the length of longest increasing subsequence of vector -arr
+*/
+
+int lis(std :: vector< int > arr, int n) {
+    std :: vector< int > lis(n);
+    int i, j, max = 0;
+    // Initialize LIS values for all indexes
+    for (i = 0; i < n; i++) {
         lis[i] = 1;
     }
-    /* Compute optimized LIS values in bottom up manner */
-    for (i = 1; i < n; i++ ){
-        for (j = 0; j < i; j++ )  {
+    // Compute optimized LIS values in bottom up manner
+    for (i = 1; i < n; i++) {
+        for (j = 0; j < i; j++) {
             if ( arr[i] > arr[j] && lis[i] < lis[j] + 1) {
                 lis[i] = lis[j] + 1;
             }
         }
     }
-    /* Pick maximum of all LIS values */
-    for (i = 0; i < n; i++ ) {
+    // Pick maximum of all LIS values
+    for (i = 0; i < n; i++) {
         if (max < lis[i]) {
             max = lis[i];
         }
     }
-    /* Free memory to avoid memory leak */
-    free(lis);
     return max;
 }
+
 #endif
--- C++/test/algorithm/dynamic_programming/longest_increasing_subsequence.cpp
+++ C++/test/algorithm/dynamic_programming/longest_increasing_subsequence.cpp
@@ -1,9 +1,13 @@
 #include "third_party/catch.hpp"
 #include "algorithm/dynamic_programming/longest_increasing_subsequence.hpp"
+
 TEST_CASE("Base Cases", "[LIS]") {
   REQUIRE(lis(std :: vector < int > ({10, 22, 9, 33, 21, 50, 41, 60}),8) == 5);
 }
 TEST_CASE("Simple Cases", "[LIS]") {
   REQUIRE(lis(std :: vector < int > ({2, 5, 3, 7, 11, 8, 10, 13, 6}),9) == 6);
   REQUIRE(lis(std :: vector < int > ({58, 10, 45, -1, 100}),5) == 3);
+  REQUIRE(lis(std :: vector < int > ({-5, 22, 9, 33, 21, 1, 41, 60 }),8) == 5);
+  REQUIRE(lis(std :: vector < int > ({-5, 22, 9, -5, 21, 1, -8, 60 }),8) == 4);
+  REQUIRE(lis(std :: vector < int > ({-5, 21, 1, -8, 60}),5) == 3);
 }
