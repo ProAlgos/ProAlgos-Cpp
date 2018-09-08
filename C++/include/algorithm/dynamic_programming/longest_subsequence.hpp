24da267146d61fdff0735337e29bb1b24e54d6bd
Cole Severson <cseverso@uoregon.edu>
2018-09-07 19:21:20 -0700

    added 4 space tabs to longest_subsequence

--- C++/include/algorithm/dynamic_programming/longest_subsequence.hpp
+++ C++/include/algorithm/dynamic_programming/longest_subsequence.hpp
@@ -27,9 +27,9 @@ O(n)
 
 int longest_subsequence(std::vector<int>& input, int order = 1, const bool to_show_state = true) {//order of 1 means increasing subsequence, -1 for decreasing subsequence
     int elements = input.size();//number of elements in the vector
-	
-	if(elements == 1 || elements == 0) 
-		return elements;//base case
+    
+    if(elements == 1 || elements == 0) 
+        return elements;//base case
  
     int array[elements];//array to store the longest increasing subsequence at each location, this can then be used to backtrack and find what was the longest sequence
     int max = 0; //max represents current largest size 
@@ -37,37 +37,37 @@ int longest_subsequence(std::vector<int>& input, int order = 1, const bool to_sh
     for (int i = 0; i < elements; i++)
         array[i] = 1; //default value
 
-	for(int i = 1; i < elements; i++)
-		for(int j = 0; j < i; j++)
-			if(((input[i] - input[j]) * order > 0) && array[i] < array[j] + 1)//if the value is greater than or less than and that element in the array has more elements in its subsequence
-				array[i] = array[i] + 1;
+    for(int i = 1; i < elements; i++)
+        for(int j = 0; j < i; j++)
+            if(((input[i] - input[j]) * order > 0) && array[i] < array[j] + 1)//if the value is greater than or less than and that element in the array has more elements in its subsequence
+                array[i] = array[i] + 1;
 
-	for(int i = 0; i < elements; i++)
-		if (array[i] > max)
-			max = array[i];
+    for(int i = 0; i < elements; i++)
+        if (array[i] > max)
+            max = array[i];
 
-	if(to_show_state) { //if this is true print out the subsequence
-		std::vector<int> sub;//this vector will be populated with the ordered subsequence
-		int counter = max;
-		for(int i = elements - 1; i > -1; i--) {
-			if(array[i] == counter)
-				if(counter == max) {//the last value in the sequence will have the highest value
-					sub.push_back(input.at(i));
-					counter--;	
-				}else if (((sub.at(0) - input.at(i)) * order > 0)) {
-					sub.push_back( input.at(i));
-					counter--;
-				}
-		}	
+    if(to_show_state) { //if this is true print out the subsequence
+        std::vector<int> sub;//this vector will be populated with the ordered subsequence
+        int counter = max;
+        for(int i = elements - 1; i > -1; i--) {
+            if(array[i] == counter)
+                if(counter == max) {//the last value in the sequence will have the highest value
+                    sub.push_back(input.at(i));
+                    counter--;  
+                }else if (((sub.at(0) - input.at(i)) * order > 0)) {
+                    sub.push_back( input.at(i));
+                    counter--;
+                }
+        }   
 
-		for(int i = max-1; i > -1; i--)
-			printf("%d, ", sub.at(i));
+        for(int i = max-1; i > -1; i--)
+            printf("%d, ", sub.at(i));
 
-		printf("\n"); 
-	}
-	
-	
-	return max;  
+        printf("\n"); 
+    }
+    
+    
+    return max;  
 }; 
 
 #endif
