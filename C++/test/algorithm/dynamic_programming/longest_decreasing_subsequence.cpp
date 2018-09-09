481310c1701aa5fa1de19aeeefc2b99c25d5ac9d
Cole Severson <cseverso@uoregon.edu>
2018-09-09 14:02:48 -0700

    fixed spacing around loops and conditionals, fixed spacing around some statements due to mistabs, fixed big O notation, removed space between end of line and the start of comment,  and fixed space between brace and else statement.

--- C++/test/algorithm/dynamic_programming/longest_decreasing_subsequence.cpp
+++ C++/test/algorithm/dynamic_programming/longest_decreasing_subsequence.cpp
@@ -11,5 +11,6 @@ TEST_CASE("Base cases", "[longest_decreasing_subsequence]") {
 TEST_CASE("Test cases", "[longest_decreasing_subsequence]") {
 	REQUIRE(longest_decreasing_subsequence(std::vector<int>({1,2,3,4,0}) == 2);
 	REQUIRE(longest_decreasing_subsequence(std::vector<int>({10,12,11,9,8,5,6,2}) == 7);
+	REQUIRE(longest_decreasing_subsequence(std::vector<int>({1,2,3,4,5,6,7,8,9,10}) == 1);
 }
 #undef LONGEST_DECREASING_SUBSEQUENCE
