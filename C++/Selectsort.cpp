1b81f16902e2462867c3c4973001738ba7717108
26Sanjana26 <34203616+26Sanjana26@users.noreply.github.com>
2018-10-05 18:17:07 +0530

    Implemented selection sort through function

---
+++ C++/Selectsort.cpp
@@ -0,0 +1,20 @@
+void selectionSort(int arr[], int n) 
+{ 
+    int i, j, min_idx; 
+  
+    // One by one move boundary of unsorted subarray 
+    for (i = 0; i < n-1; i++) 
+    { 
+        // Find the minimum element in unsorted array 
+        min_idx = i; 
+        for (j = i+1; j < n; j++) 
+          if (arr[j] < arr[min_idx]) 
+            min_idx = j; 
+  
+        // Swap the found minimum element with the first element 
+            
+          int temp =arr[min_idx]; 
+          arr[min_idx] = arr[i]; 
+         arr[i] = temp; 
+    } 
+} 
