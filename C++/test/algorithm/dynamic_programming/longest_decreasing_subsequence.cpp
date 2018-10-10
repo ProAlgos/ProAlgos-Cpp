8f863ab7bd691b332ff9dfc37d2c824ebd930ef0
Cole Severson <cseverso@uoregon.edu>
2018-10-10 13:39:45 -0700

    Removed header guard from .cpp file

--- C++/test/algorithm/dynamic_programming/longest_decreasing_subsequence.cpp
+++ C++/test/algorithm/dynamic_programming/longest_decreasing_subsequence.cpp
@@ -1,5 +1,3 @@
-#define LONGEST_DECREASING_SUBSEQUENCE
-
 #include "third_party/catch.hpp"
 #include "include/algorithms/dynamic_programming/longest_decreasing_subsequence.hpp"
 
@@ -13,4 +11,3 @@ TEST_CASE("Test cases", "[longest_decreasing_subsequence]") {
 	REQUIRE(longest_decreasing_subsequence(std::vector<int>({10,12,11,9,8,5,6,2}) == 7);
 	REQUIRE(longest_decreasing_subsequence(std::vector<int>({1,2,3,4,5,6,7,8,9,10}) == 1);
 }
-#undef LONGEST_DECREASING_SUBSEQUENCE
