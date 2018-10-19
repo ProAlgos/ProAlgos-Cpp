f1af0d0f88fcfffbf7bb2fc2ac40a76092e7ab7a
Cooper Timmerman <coopert925@gmail.com>
2018-10-18 17:02:05 -0700

    Cleaned up code

--- C++/test/number_theory/extended_euclid.test.cpp
+++ C++/test/number_theory/extended_euclid.test.cpp
@@ -4,15 +4,12 @@
 #include "source/number_theory/extended_euclid.cpp"
 
 TEST_CASE("Base Cases","[extended_euclidean]") {
-
 	REQUIRE(extended_euclidean(0,1) == (array<int,2>{0,1}));
 	REQUIRE(extended_euclidean(1,2) == (array<int,2>{1,0}));
 	REQUIRE(extended_euclidean(1,99999) == (array<int,2>{1,0}));
-
 }
 
 TEST_CASE("Normal Cases","[extended_euclidean]") {
-
 	REQUIRE(extended_euclidean(10,30) == (array<int,2>{1,0}));
 	REQUIRE(extended_euclidean(105,15) == (array<int,2>{0,1}));
 	REQUIRE(extended_euclidean(9243,45) == (array<int,2>{-2,411}));
@@ -20,7 +17,6 @@ TEST_CASE("Normal Cases","[extended_euclidean]") {
 	REQUIRE(extended_euclidean(350,64) == (array<int,2>{15,-82}));
 	REQUIRE(extended_euclidean(8056,256) == (array<int,2>{15,-472}));
 	REQUIRE(extended_euclidean(2468024,864226) == (array<int,2>{-161525,461277}));
-
 }
 
 #undef EXTENDED_EUCLID_TEST
