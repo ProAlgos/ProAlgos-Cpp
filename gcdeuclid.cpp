da155ec8fa815b59cbe2e77da01469c8e7fbe768
Apoorva <Apoorva>
2018-10-26 23:21:09 +0530

    Euclidean Algorithm

---
+++ gcdeuclid.cpp
@@ -0,0 +1,25 @@
+#include<string>
+#include<algorithm>
+#include<bits/stdc++.h>
+#include <ctime> 
+ 
+
+using namespace std;
+
+
+int gcd(int a,int b)
+{
+	if(a==0)
+	return b;
+
+	else
+	return (b%a,a);
+}
+
+
+int main()
+{
+	int a,b;
+	cin>>a>>b;
+	cout<<"GCD of "<<a<<" and "<<b<<" = "<<gcd(a,b);
+}
