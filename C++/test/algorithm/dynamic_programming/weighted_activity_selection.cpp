b8605e66ea863f79672d5816dcf448dc9091d5b6
Alex Johnson <alxmjo@gmail.com>
2019-08-01 21:11:49 -0700

    Modify whitespace

--- C++/test/algorithm/dynamic_programming/weighted_activity_selection.cpp
+++ C++/test/algorithm/dynamic_programming/weighted_activity_selection.cpp
@@ -7,9 +7,10 @@ TEST_CASE("Base cases", "[weighted_activity]") {
 }
 
 TEST_CASE("Medium cases", "[weighted_activity]") {
-    REQUIRE(weighted_activity( { 1, 3, 6, 2}, { 2, 5, 19, 100 }, { 50, 20, 100, 200 } ) == 250);
+    REQUIRE(weighted_activity( { 1, 3, 6, 2}, 
+                               { 2, 5, 19, 100 }, 
+                               { 50, 20, 100, 200 } ) == 250);
     REQUIRE(weighted_activity( { 5, 3, 10, 8, 3, 8, 8, 8, 10, 10 }, 
                                { 6, 5, 11, 9, 9, 9,10, 9, 11, 11 }, 
                                { 10, 4, 10, 5, 8, 5, 4, 3, 2, 1  } ) == 29);
-
 }
