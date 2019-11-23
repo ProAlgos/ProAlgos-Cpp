c5642803e4e652f70e440d82f7b4810593980d54
Paulo R <paulorcb2011@hotmail.com>
2019-11-23 16:41:15 -0300

    Add Coin Change Problem

--- C++/include/algorithm/dynamic_programming/coin_change.hpp
+++ C++/include/algorithm/dynamic_programming/coin_change.hpp
@@ -1,8 +1,10 @@
 /*
     Coin Change Algorithm
     ------------------------------------
-
+    
+    Given an unlimited amount of coins with different values, find the number of ways of making change for an given value using the coins. 
     Time complexity
+    
     ----------------
     O(M*N), where M is the number of coins with different values and N is the amount that we desired to change
 
@@ -18,11 +20,11 @@
 
 using std::vector;
 
-int coin_change(const vector<int> coin, int number_of_coins, int amount)
+int coin_change(const vector<int>& coin, int number_of_coins, int amount)
 {
-    if(number_of_coins == 0 && amount == 0) {
+    if (number_of_coins == 0 && amount == 0) {
         return 1;
-    } else if(number_of_coins == 0 && amount != 0){
+    } else if (number_of_coins == 0) {
         return 0;
     }
 
@@ -35,24 +37,24 @@ int coin_change(const vector<int> coin, int number_of_coins, int amount)
     int current_in; // stores the number of ways in which the amount can be changed including the current coin value
     int current_out; // stores the number of ways in which the amount can be changed excluding the current coin value
 
-    for(j = 0; j < number_of_coins; j++) {
+    for (j = 0; j < number_of_coins; j++) {
         table[0][j] = 1;
     }
 
     // i represents the amount whose change is required
     // j represents the coin values available
-    for(i = 1; i <= amount; i++) {
-        for(j = 0; j < number_of_coins; j++) {
+    for (i = 1; i <= amount; i++) {
+        for (j = 0; j < number_of_coins; j++) {
             // if the the value of current coin is less than or equal to total amount whose change is required
             // include this coin
-            if(i >= coin[j]) {
+            if (i >= coin[j]) {
                 current_in = table[i - coin[j]][j];
             } else {
                 current_in = 0;
             }
 
 
-            if(j >= 1) {
+            if (j >= 1) {
                 current_out = table[i][j - 1];
             } else {
                 current_out = 0;
