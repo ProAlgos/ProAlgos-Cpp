636654950e051552e2577f2bde5f5d0337a3a278
Ari Greenberg <ari.greenberg99@gmail.com>
2020-01-22 07:47:02 +0200

    fixing function names

--- C++/test/data_structure/heap/min_heap.cpp
+++ C++/test/data_structure/heap/min_heap.cpp
@@ -9,81 +9,81 @@ TEST_CASE("Verify initial state", "[min-heap]") {
 
 TEST_CASE("Building a heap from inserts", "[min-heap]") {
   MinHeap<int> heap;
-  heap.insertValue(1);
-  heap.insertValue(8);
-  heap.insertValue(6);
-  heap.insertValue(5);
-  heap.insertValue(13);
-  heap.insertValue(9);
-  heap.insertValue(4);
-  heap.insertValue(517);
-  heap.insertValue(-23);
-  heap.insertValue(0);
+  heap.insert_value(1);
+  heap.insert_value(8);
+  heap.insert_value(6);
+  heap.insert_value(5);
+  heap.insert_value(13);
+  heap.insert_value(9);
+  heap.insert_value(4);
+  heap.insert_value(517);
+  heap.insert_value(-23);
+  heap.insert_value(0);
   vector<int> expected = {-23, 0, 4, 5, 1, 9, 6, 517, 8, 13};
 
-  REQUIRE(heap.returnElems() == expected);
+  REQUIRE(heap.return_elements() == expected);
 }
 
 TEST_CASE("Building a heap from vector", "[min-heap]") {
   MinHeap<int> heap({1, 8, 6, 5, 13, 9, 4, 517, -23, 0});
 
   vector<int> expected = {-23, 0, 4, 5, 1, 9, 6, 517, 8, 13};
-  REQUIRE(heap.returnElems() == expected);
+  REQUIRE(heap.return_elements() == expected);
 }
 
 TEST_CASE("Removing from a heap", "[min-heap]") {
   MinHeap<int> heap({1, 8, 6, 5, 13, 9, 4, 517, -23, 0});
-  heap.removeValue(8);
+  heap.remove_value(8);
 
   vector<int> expected = {-23, 0, 4, 5, 1, 9, 6, 517, 13};
-  REQUIRE(heap.returnElems() == expected);
+  REQUIRE(heap.return_elements() == expected);
 
-  heap.removeValue(-23);
+  heap.remove_value(-23);
 
   vector<int> expected2 = {0, 1, 4, 5, 13, 9, 6, 517};
-  REQUIRE(heap.returnElems() == expected2);
+  REQUIRE(heap.return_elements() == expected2);
 
-  heap.removeValue(8);
+  heap.remove_value(8);
 
   vector<int> expected3 = {0, 1, 4, 5, 13, 9, 6, 517};
-  REQUIRE(heap.returnElems() == expected3);
+  REQUIRE(heap.return_elements() == expected3);
 }
 
 TEST_CASE("Complex removals and insertions", "[min-heap]") {
   MinHeap<int> heap({5, 2, 89, 901, 563, 120, 12, 13, -46, 53, -89});
   
   vector<int> expected = {-89, -46, 12, 5, 2, 120, 89, 13, 901, 53, 563};
-  REQUIRE(heap.returnElems() == expected);
+  REQUIRE(heap.return_elements() == expected);
 
-  heap.insertValue(10);
+  heap.insert_value(10);
 
   vector<int> expected2 = {-89, -46, 10, 5, 2, 12, 89, 13, 901, 53, 563, 120};
-  REQUIRE(heap.returnElems() == expected2);
+  REQUIRE(heap.return_elements() == expected2);
 
-  heap.insertValue(-100);
+  heap.insert_value(-100);
 
   vector<int> expected3 = {-100, -46, -89, 5, 2, 10, 89, 13, 901, 53, 563, 120, 12};
-  REQUIRE(heap.returnElems() == expected3);
+  REQUIRE(heap.return_elements() == expected3);
 
-  heap.removeValue(-46);
+  heap.remove_value(-46);
 
   vector<int> expected4 = {-100, 2, -89, 5, 12, 10, 89, 13, 901, 53, 563, 120};
-  REQUIRE(heap.returnElems() == expected4);
+  REQUIRE(heap.return_elements() == expected4);
 
-  heap.insertValue(11);
+  heap.insert_value(11);
 
   vector<int> expected5 = {-100, 2, -89, 5, 12, 10, 89, 13, 901, 53, 563, 120, 11};
-  REQUIRE(heap.returnElems() == expected5);
+  REQUIRE(heap.return_elements() == expected5);
 
-  heap.removeValue(563);
+  heap.remove_value(563);
 
   vector<int> expected6 = {-100, 2, -89, 5, 11, 10, 89, 13, 901, 53, 12, 120};
-  REQUIRE(heap.returnElems() == expected6);
+  REQUIRE(heap.return_elements() == expected6);
 
-  heap.removeValue(-100);
+  heap.remove_value(-100);
 
   vector<int> expected7 = {-89, 2, 10, 5, 11, 120, 89, 13, 901, 53, 12};
-  REQUIRE(heap.returnElems() == expected7);
+  REQUIRE(heap.return_elements() == expected7);
 
-  REQUIRE(heap.returnMin() == -89);
+  REQUIRE(heap.return_min() == -89);
 }
