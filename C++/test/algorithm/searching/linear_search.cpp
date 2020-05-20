c556d35916c2f5df66c307c4d204bf878667efd4
Muhammad Zahalqa <m@tryfinally.com>
2020-05-20 14:57:57 +0300

    adapt test for linear search

--- C++/test/algorithm/searching/linear_search.cpp
+++ C++/test/algorithm/searching/linear_search.cpp
@@ -5,9 +5,9 @@ using std::vector;
 using std::string;
 
 TEST_CASE("Base cases", "[searching][linear_search]") {
-    REQUIRE(linear_search(0, vector<int>()) == -1);
+    REQUIRE(linear_search(0, vector<int>()) == 0);
     REQUIRE(linear_search(1, vector<int>({1})) == 0);
-    REQUIRE(linear_search(5, vector<int>({0})) == -1);
+    REQUIRE(linear_search(5, vector<int>({0})) == 1);
 }
 
 TEST_CASE("Integer cases", "[searching][linear_search]") {
@@ -15,7 +15,7 @@ TEST_CASE("Integer cases", "[searching][linear_search]") {
     REQUIRE(linear_search(-15, vector<int>({9, -15, 0, 3, 5})) == 1);
     REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2, 0})) == 6);
     REQUIRE(linear_search(-0, vector<int>({0, -5, 25, 8, 5, 2, 6, 3})) == 0);
-    REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2})) == -1);
+    REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2})) == 7);
 }
 
 
