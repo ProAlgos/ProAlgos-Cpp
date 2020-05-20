87439d9489167685b710f1e9303461b4b893c809
Muhammad Zahalqa <m@tryfinally.com>
2020-05-20 15:18:30 +0300

    Fix linear search unit test

--- C++/test/algorithm/searching/linear_search.cpp
+++ C++/test/algorithm/searching/linear_search.cpp
@@ -15,7 +15,7 @@ TEST_CASE("Integer cases", "[searching][linear_search]") {
     REQUIRE(linear_search(-15, vector<int>({9, -15, 0, 3, 5})) == 1);
     REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2, 0})) == 6);
     REQUIRE(linear_search(-0, vector<int>({0, -5, 25, 8, 5, 2, 6, 3})) == 0);
-    REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2})) == 7);
+    REQUIRE(linear_search(0, vector<int>({1, 29, 6, 8, 5, 2})) == 6);
 }
 
 
