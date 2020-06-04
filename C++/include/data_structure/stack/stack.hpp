72bd634b5573138e34e79fc72a6a5ae089bf5fec
Afraz Hasan <afraz@umich.edu>
2020-06-04 18:48:47 -0400

    Compiler error fix.

--- C++/include/data_structure/stack/stack.hpp
+++ C++/include/data_structure/stack/stack.hpp
@@ -9,6 +9,7 @@
 #define stack_h
 
 #include "data_structure/linked_list/singly_linked_list.hpp"
+#include <iostream>
 
 template<class T>
 class Stack {
@@ -51,8 +52,8 @@ void Stack<T>::pop() {
         return;
     }
     Node<T> * temp = head;
-    head = temp->next;
-    temp->next = nullptr;
+    head = temp->get_next();
+    temp->set_next(nullptr);
     delete temp;
     --size;
 }
@@ -64,9 +65,9 @@ template<class T>
 T Stack<T>::top() {
     if (!head) {
         std::cout << "Stack is empty\n";
-        exit(1);
+//        exit(1);
     }
-    return head->value;
+    return head->get_value();
 }
 
 /*
