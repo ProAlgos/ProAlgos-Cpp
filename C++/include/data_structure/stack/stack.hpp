dbc24cd8f505f78fe033e7356bb8ae2ae09b5d8e
Afraz Hasan <afraz@umich.edu>
2020-06-05 15:22:51 -0400

    Fixed formatting errors. Added isEmpty() method.

--- C++/include/data_structure/stack/stack.hpp
+++ C++/include/data_structure/stack/stack.hpp
@@ -5,24 +5,25 @@
     and removed from the top(head). Only the top element can be accessed at any given time.
     Hence, there is no random access. All member functions have O(1) time complexity.
 */
-#ifndef stack_h
-#define stack_h
+
+#ifndef STACK_HPP
+#define STACK_HPP
 
 #include "data_structure/linked_list/singly_linked_list.hpp"
-#include <iostream>
 
 template<class T>
 class Stack {
 private:
-    int size;
-    Node<T> * head;
+    size_t size;
+    Node<T>* head;
     
 public:
     Stack();
     void push(const T &item);
     void pop();
     T top();
-    int length();
+    size_t length();
+    bool isEmpty();
 };
 
 /*
@@ -33,25 +34,21 @@ template<class T>
 Stack<T>::Stack() : size {0}, head {nullptr} {}
 
 /*
-    Adds item to the top of the stack
- */
+ Adds an item to the top of the stack
+*/
 template<class T>
 void Stack<T>::push(const T &item) {
-    Node<T> * temp  = new Node<T>(item, head);
+    Node<T>* temp  = new Node<T>(item, head);
     head = temp;
     ++size;
 }
 
 /*
-    Removes an item from the top of the stack
+ Removes an item from the top of the stack
  */
 template<class T>
 void Stack<T>::pop() {
-    if (!head) {
-        std::cout << "Stack is empty\n";
-        return;
-    }
-    Node<T> * temp = head;
+    Node<T>* temp = head;
     head = temp->get_next();
     temp->set_next(nullptr);
     delete temp;
@@ -59,23 +56,26 @@ void Stack<T>::pop() {
 }
 
 /*
-   Returns the top element of the stack
+ Returns the top element of the stack
 */
 template<class T>
 T Stack<T>::top() {
-    if (!head) {
-        std::cout << "Stack is empty\n";
-//        exit(1);
-    }
     return head->get_value();
 }
 
 /*
-   Returns the size of the stack
+ Returns the size of the stack
 */
 template<class T>
-int Stack<T>::length() {
+size_t Stack<T>::length() {
     return size;
 }
 
-#endif /* stack_h */
+/*
+ Returns true if stack is empty
+*/
+bool isEmpty() {
+    return size == 0;
+}
+
+#endif /* STACK_HPP */
