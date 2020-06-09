d481b72ab34414fb279cf13d07f86af06c46759b
nseabourn <nseabourn001@gmail.com>
2020-06-09 14:28:46 -0700

    Add primorial_n(), add test cases for primorial_n(), adjust test cases for primorial_Pn()

--- C++/include/algorithm/number_theory/primorial.hpp
+++ C++/include/algorithm/number_theory/primorial.hpp
@@ -21,8 +21,11 @@
 
 typedef unsigned long long int ULL;
 
-// primorial(Pn) where Pn > 47 goes beyond the range of ULL
+// primorial_Pn(Pn) where Pn > 47 goes beyond the range of ULL
 const unsigned int MAX_P_N = 47;
+
+// primorial_n(n) where n > 15 goes beyond the range of ULL
+const unsigned int MAX_N = 15;
 
 // returns 0 if Pn is not a prime number or if Pn# is too big to fit in an 
 // ULL. returns the primorial Pn# otherwise
@@ -57,6 +60,39 @@ ULL primorial_Pn(unsigned int p_n) {
     // make sure Pn was a prime nubmer
     if (primes[primes.size()-1] != p_n) {
         return 0;
+    }
+
+    return product;
+}
+
+// returns 0 if n is 0 or if Pn# is too big to fit in an ULL. 
+// returns the primorial Pn# otherwise
+ULL primorial_n(unsigned int n) {
+    if (n < 1) {
+        return 0;
+    }
+
+    std::vector<unsigned int> primes;
+    
+    ULL product = 1;
+
+    for (unsigned int i = 2; primes.size() < n; i++) {
+        bool is_prime = true;
+        for (std::vector<unsigned int>::iterator it = primes.begin(); is_prime
+            && it != primes.end(); it++) {
+            if (i % *it == 0) {
+                is_prime = false;
+            }
+        }
+        if (is_prime) {            
+            // check for unsigned integer wraparound
+            if (ULLONG_MAX / product < i) {
+                return 0;
+            } else {
+                product *= i;
+                primes.push_back(i);
+            }
+        }
     }
 
     return product;
--- C++/test/algorithm/number_theory/primorial.cpp
+++ C++/test/algorithm/number_theory/primorial.cpp
@@ -3,6 +3,7 @@
 
 TEST_CASE("Base cases", "[primorial]") {
     REQUIRE(primorial_Pn(2) == 2);
+    REQUIRE(primorial_n(1) == 2);
 }
 
 TEST_CASE("Normal cases", "[primorial]") {
@@ -12,6 +13,12 @@ TEST_CASE("Normal cases", "[primorial]") {
     REQUIRE(primorial_Pn(11) == 2310);
     REQUIRE(primorial_Pn(13) == 30030);
     REQUIRE(primorial_Pn(MAX_P_N) == 614889782588491410); // MAX_P_N = 47
+    REQUIRE(primorial_n(2) == 6);
+    REQUIRE(primorial_n(3) == 30);
+    REQUIRE(primorial_n(4) == 210);
+    REQUIRE(primorial_n(5) == 2310);
+    REQUIRE(primorial_n(6) == 30030);
+    REQUIRE(primorial_n(MAX_N) == 614889782588491410); // MAX_N = 15
 }
 
 TEST_CASE("Negative cases", "[primorial]") {
@@ -22,10 +29,14 @@ TEST_CASE("Negative cases", "[primorial]") {
     REQUIRE(primorial_Pn(9) == 0);
     REQUIRE(primorial_Pn(12) == 0);
     REQUIRE(primorial_Pn(46) == 0);
-    REQUIRE(primorial_Pn(UINT_MAX) == 0);
+    REQUIRE(primorial_n(0) == 0);
 }
 
 TEST_CASE("Wraparound cases", "[primorial]") {
+    REQUIRE(primorial_Pn(MAX_P_N + 1) == 0);
     REQUIRE(primorial_Pn(53) == 0);
     REQUIRE(primorial_Pn(173) == 0);
+    REQUIRE(primorial_Pn(UINT_MAX) == 0);
+    REQUIRE(primorial_n(MAX_N + 1) == 0);
+    REQUIRE(primorial_n(40) == 0);
 }
