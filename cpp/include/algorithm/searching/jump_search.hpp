e5ad20046c8227c3fb04792c39c469a0227859d8
Chaotyc <chao_tyc@gmx.de>
2021-01-03 22:29:47 +0100

    Implement the Jump Search Algorithm and write Test Cases for it.

--- cpp/include/algorithm/searching/jump_search.hpp
+++ cpp/include/algorithm/searching/jump_search.hpp
@@ -1,6 +1,49 @@
+/*
+    Jump search
+    -------------
+    A searching algorithm that finds the index of a target value within a
+    sorted array using fixed step jumps to skip elements.
 
+    Time complexity
+    ---------------
+    O(sqrt(N)), where N is the number of elements in the array.
+
+    Space complexity
+    ----------------
+    O(1).
+*/
 
 #ifndef PROALGOS_CPP_JUMP_SEARCH_HPP
 #define PROALGOS_CPP_JUMP_SEARCH_HPP
+
+#include <vector>
+#include <cmath>
+#include <cstdio>
+
+/*
+    jump_search
+    -------------
+    Returns the index where a given element is found in an ascending sorted array.
+    If the element is not found, it returns -1.
+*/
+
+template <typename T>
+int jump_search(const T& element, const std::vector<T>& values){
+    int stepSize = (int) sqrt(values.size());
+    int prevIndex = 0;
+
+    for (int i = 0; i < (int) values.size(); i+=stepSize){
+        if(values[i] >= element){
+            for(int j = prevIndex; j <= i; j++){
+                if(values[j] == element){
+                    return j;
+                }
+            }
+            return -1;
+        }
+        prevIndex = i;
+    }
+    return -1;
+}
 
 #endif //PROALGOS_CPP_JUMP_SEARCH_HPP
--- cpp/test/algorithm/searching/jump_search.cpp
+++ cpp/test/algorithm/searching/jump_search.cpp
@@ -1,2 +1,19 @@
 #include "third_party/catch.hpp"
 #include "algorithm/searching/jump_search.hpp"
+
+using std::vector;
+using std::string;
+
+TEST_CASE("Base cases", "[searching][linear_search]") {
+    REQUIRE(jump_search(0, vector<int>()) == -1);
+    REQUIRE(jump_search(1, vector<int>({1})) == 0);
+    REQUIRE(jump_search(5, vector<int>({0})) == -1);
+}
+
+TEST_CASE("Integer cases", "[searching][linear_search]") {
+    REQUIRE(jump_search(5, vector<int>({0, 5, 25, 50, 60, 67, 80, 81})) == 1);
+    REQUIRE(jump_search(-15, vector<int>({-45, -44, -33, -15, 0, 3, 5})) == 3);
+    REQUIRE(jump_search(42, vector<int>({1, 2, 6, 8, 20, 22, 42})) == 6);
+    REQUIRE(jump_search(-0, vector<int>({-12, -11, -5, -3, 0, 3, 6, 9})) == 4);
+    REQUIRE(jump_search(0, vector<int>({1, 2, 3, 4, 5, 6})) == -1);
+}
