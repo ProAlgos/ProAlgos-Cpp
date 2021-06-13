17c867cbddd18d392073977e2989cbb269bc38fc
Syed Faheel Ahmad <faheel@live.in>
2021-06-14 00:03:56 +0530

    Refactor code and update comments

--- cpp/include/algorithm/searching/interpolation_search.hpp
+++ cpp/include/algorithm/searching/interpolation_search.hpp
@@ -1,15 +1,20 @@
 /*
-    Interpolation Search
-    -------------
-    A similar algorithm to Binary Search. However, instead of always going for the middle element, 
-    interpolation search may go to different locations according to the value of the key being searched.
+    Interpolation search
+    --------------------
+    Interpolation search is an improvement over binary search for
+    instances where the values in a sorted array are uniformly distributed.
+
+    Binary search always goes to the middle element whereas interpolation
+    search may go to different positions according to the value of being
+    searched. For example, if the value is closer to the last element,
+    interpolation search is likely to start search towards the end of the
+    array.
 
     Time complexity
     ---------------
-    O(log2(log2 n)): for the average case, 
-    O(n) for the worst case (when items are distributed exponentially), 
-
-    where n is the number of elements in the array.
+    Average case: O(log2(log2(N)))
+    Worst case: O(N) [when items are distributed exponentially]
+    where N is the number of elements in the array.
 
     Space complexity
     ----------------
@@ -20,45 +25,48 @@
 #define INTERPOLATION_SEARCH_HPP
 
 #include <vector>
+
 /*
     interpolation_search
-    -------------
-    Firstly, the value of `pos` is calculated. it is the position where t
-    he algorithm  will look for the value
-    If it is a match, return the index and exit. 
-    If the item is less than sorted_values[pos], calculate the probe position of the left sub-array. 
-    Else calculate the same in the right sub-array.
-    Repeat until a match is found or the sub-array reduces to zero. If so, return -1.
+    --------------------
+    In a loop, calculate the value of `pos` using the probe position formula:
+    a. If it is a match, return the index.
+    b. If `value` is less than `sorted_values[pos]`, calculate the probe
+       position of the left sub-array. Otherwise calculate the same in the
+       right sub-array.
+    c. Repeat until a match is found or the sub-array reduces to zero. Return
+       -1 if no match is found.
 */
-
 template <typename T>
 int interpolation_search(const T& value, const std::vector<T>& sorted_values) {
-    // Find indexes of two corners
-    int n = sorted_values.size();
-    int l = 0, r = (n - 1);
+    int left = 0, right = sorted_values.size() - 1;
 
-    while (l <= r && value >= sorted_values[l] && value <= sorted_values[r]) {
-        if (l == r) {
-            if (sorted_values[l] == value) {
-                return l;
+    while (left <= right and value >= sorted_values[left] and value <= sorted_values[right]) {
+        if (left == right) {
+            if (sorted_values[left] == value) {
+                return left;
             }
-            return -1;          //since left and right are at the same index, and that index does not match with the value, it means the value does not exist.
+            // value was not found, since `left` and `right` are the same index
+            // and the value does not exist at that index
+            return -1;
         }
 
-        // Probing the position
-        int pos = l + (((double)(r - l) / (sorted_values[r] - sorted_values[l])) * (value - sorted_values[l]));        //uniform distribution kept in mind.
+        // calculate the probe position (assuming uniform distribution of values)
+        float slope = (float)(sorted_values[right] - sorted_values[left]) / (right - left)
+        int pos = left + (value - sorted_values[left]) / slope;
 
         if (sorted_values[pos] == value) {
             return pos;
         }
-
-        if (sorted_values[pos] > value) {
-            r = pos - 1;        //this means value is in left subarray
-        } else {
-            l = pos + 1;        //this means value is in right subarray
+        else if (value < sorted_values[pos]) {
+            right = pos - 1;    // value might be in the left subarray
+        }
+        else {
+            left = pos + 1;     // value might be in the right subarray
         }
     }
-    return -1;                  // value not found
+
+    return -1;  // value was not found
 }
 
-#endif // INTERPOLATION_SEARCH_HPP
+#endif  // INTERPOLATION_SEARCH_HPP
