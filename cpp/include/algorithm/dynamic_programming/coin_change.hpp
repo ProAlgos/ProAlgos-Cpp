692a2740f2a8f0e7bab15b73b01f02fc2151bf8e
Dileep Kumar Yadav <67112675+Dileep126@users.noreply.github.com>
2021-08-22 00:47:07 +0530

    Update coin_change.hpp
    The space complexity of the coin change problem has been improved from O(N*M) to O(N).

--- cpp/include/algorithm/dynamic_programming/coin_change.hpp
+++ cpp/include/algorithm/dynamic_programming/coin_change.hpp
@@ -10,7 +10,7 @@
 
     Space complexity
     ----------------
-    O(M*N), where M is the number of coins with different values and N is the amount that we desired to change
+    O(N), where M is the number of coins with different values and N is the amount that we desired to change
 */
 
 #ifndef COIN_CHANGE_HPP
@@ -22,49 +22,24 @@ using std::vector;
 
 int coin_change(const vector<int>& coin, int number_of_coins, int amount)
 {
-    if (number_of_coins == 0 && amount == 0) {
-        return 1;
-    } else if (number_of_coins == 0) {
-        return 0;
-    }
+    // Make an array of size (amount+1), to store the computed ways for various amount values from (0 to amount)
+    // dp[i] stores the number of ways amount 'i' can be made using the given set of coins.
+    vector<int>dp(amount+1, 0);
+    
+    dp[0]=1;
 
     int i, j;
 
-    // Make a matrix of size (amount+1)*number_of_coins to tabulate the computed values
-    // table[i][j] represents no.of ways in which an 'i' can be made with just j type of coins available
-    vector<vector<int>> table (amount + 1, vector<int>(number_of_coins, 0));
-
-    int current_in; // Stores the number of ways in which the amount can be changed including the current coin value
-    int current_out; // Stores the number of ways in which the amount can be changed excluding the current coin value
-
-    for (j = 0; j < number_of_coins; j++) {
-        table[0][j] = 1;
-    }
-
-    // i represents the amount whose change is required
-    // j represents the coin values available
-    for (i = 1; i <= amount; i++) {
-        for (j = 0; j < number_of_coins; j++) {
-            // If the the value of current coin is less than or equal to total amount whose change is required
-            // include this coin
-            if (i >= coin[j]) {
-                current_in = table[i - coin[j]][j];
-            } else {
-                current_in = 0;
-            }
-
-
-            if (j >= 1) {
-                current_out = table[i][j - 1];
-            } else {
-                current_out = 0;
-            }
-
-            table[i][j] = current_in + current_out;
+    // i traverses on the coin array
+    // j represents the amount whose change is required
+    for(int i=0;i<number_of_coins;i++){
+        for(int j=coin[i];j<=amount;j++){
+            // For each amount value, sum up the total number of ways of making the change with a particular denomination.
+            dp[j]=dp[j]+dp[j-coin[i]];
         }
     }
 
-    return table[amount][number_of_coins - 1];
+    return dp[amount];
 }
 
 #endif // COIN_CHANGE_HPP
