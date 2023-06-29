f5f280b5c37b48da6b64b62d1287e480d3948978
pm100 <paulmoore100@hotmail.com>
2023-06-29 11:24:22 -0700

    binary search fix

--- cpp/include/algorithm/searching/binary_search.hpp
+++ cpp/include/algorithm/searching/binary_search.hpp
@@ -30,11 +30,12 @@
 template <typename T>
 int binary_search(const T& value, const std::vector<T>& sorted_values,
         const int low, const int high) {
+    
     int mid = low + (high - low) / 2;
 
     if (value == sorted_values[mid])
         return mid;
-    else if (low <= high) {
+    else if (low < high) {
         if (value < sorted_values[mid]) {
             // value must be between indices low and mid-1, if exists
             return binary_search(value, sorted_values, low, mid - 1);
